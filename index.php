cce6955515b8b42ef7b34b153e789012132aceb1
elle <lolita.s.krievs@wmich.edu>
2011-01-25 19:29:31 -0500

    Project 01: comments into doc blocks

--- index.php
+++ index.php
@@ -16,24 +16,32 @@
  * @since File available since Release 0.0.0
  */ 
 
-/** Turn on output buffering
+/** 
+ * Turn on output buffering
  *  While output buffering is active no output is sent from the script 
  * (other than headers), instead the output is stored in an internal buffer.
  */
 ob_start();
 
-/** When a string is literal (contains no variable substitutions), 
+/** 
+ * When a string is literal (contains no variable substitutions), 
  * the apostrophe or "single quote" should always be used to demarcate the string:
  */
 echo 'My First Project with Output Buffering';
 
-/** Copy the contents of the internal buffer 
+/** 
+ * Copy the contents of the internal buffer 
  * into a string variable using ob_get_contents().
  */
 $v = ob_get_contents();
 
-// clean (erase) the output buffer and turn off output buffering
+/**
+ * clean (erase) the output buffer and turn off output buffering
+ */
 ob_end_clean();
 
-// echo the contents of the internal buffer
+/**
+ *  echo the contents of the internal buffer
+ */
+
 echo $v;
