c767de390276402574148117ba243783a755d838
elle <lolita.s.krievs@wmich.edu>
2011-01-25 19:11:09 -0500

    project 01 revisited

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
  * 
  * LICENSE: as fit
  * 
+ * @author Elle Krievs
  * @category Anm293
- * @package  
+ * Project 01 
  * @copyright Copyright (c) 2011 me
  * @license ...
  * @version  0.001
@@ -15,18 +16,20 @@
  * @since File available since Release 0.0.0
  */ 
 
-// Turn on output buffering
-// While output buffering is active no output is sent from the script 
-// (other than headers), instead the output is stored in an internal buffer.
-
+/** Turn on output buffering
+ *  While output buffering is active no output is sent from the script 
+ * (other than headers), instead the output is stored in an internal buffer.
+ */
 ob_start();
 
-// When a string is literal (contains no variable substitutions), 
-// the apostrophe or "single quote" should always be used to demarcate the string:
+/** When a string is literal (contains no variable substitutions), 
+ * the apostrophe or "single quote" should always be used to demarcate the string:
+ */
 echo 'My First Project with Output Buffering';
 
-// Copy the contents of the internal buffer 
-// into a string variable using ob_get_contents().
+/** Copy the contents of the internal buffer 
+ * into a string variable using ob_get_contents().
+ */
 $v = ob_get_contents();
 
 // clean (erase) the output buffer and turn off output buffering
